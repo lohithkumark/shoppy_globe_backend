820de956d33f83595d3315c11885b04e40a8e093
lohithkumar <lohitkumar2003@gmail.com>
2026-02-27 16:18:53 +0530

    Added PUT /products/:id endpoint

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -31,7 +31,6 @@ const createProduct = async (req, res) => {
 };
 
 
-
 // GET /products/:id
 const getProductById = async (req, res) => {
   try {
@@ -47,6 +46,26 @@ const getProductById = async (req, res) => {
   }
 };
 
+
+// PUT /products/:id
+const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Invalid Product ID" });
+  }
+};
+
 module.exports = {
-  getAllProducts, createProduct , getProductById
+  getAllProducts, createProduct , getProductById , updateProduct
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllProducts , createProduct , getProductById} = require("../controllers/product.controller");
+const { getAllProducts , createProduct , getProductById , updateProduct} = require("../controllers/product.controller");
 
 // GET /products
 router.get("/", getAllProducts);
 router.post("/", createProduct);
 router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
 
 module.exports = router;
