3fe6305a4d26f7f685129cbae88b65c8c2dfb134
lohithkumar <lohitkumar2003@gmail.com>
2026-02-27 16:03:46 +0530

    Added create product API (POST /products)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -10,6 +10,26 @@ const getAllProducts = async (req, res) => {
   }
 };
 
+// POST /products
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, description, stock } = req.body;
+
+    const newProduct = new Product({
+      name,
+      price,
+      description,
+      stock,
+    });
+
+    const savedProduct = await newProduct.save();
+
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
 module.exports = {
-  getAllProducts,
+  getAllProducts, createProduct
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllProducts } = require("../controllers/product.controller");
+const { getAllProducts , createProduct} = require("../controllers/product.controller");
 
 // GET /products
 router.get("/", getAllProducts);
+router.post("/", createProduct);
 
 module.exports = router;
