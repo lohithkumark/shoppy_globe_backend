93225f9beee547168b106c4efaf47786aa3c103e
lohithkumar <lohitkumar2003@gmail.com>
2026-02-27 16:09:05 +0530

    Added GET /products/:id endpoint

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -30,6 +30,23 @@ const createProduct = async (req, res) => {
   }
 };
 
+
+
+// GET /products/:id
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Invalid Product ID" });
+  }
+};
+
 module.exports = {
-  getAllProducts, createProduct
+  getAllProducts, createProduct , getProductById
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllProducts , createProduct} = require("../controllers/product.controller");
+const { getAllProducts , createProduct , getProductById} = require("../controllers/product.controller");
 
 // GET /products
 router.get("/", getAllProducts);
 router.post("/", createProduct);
+router.get("/:id", getProductById);
 
 module.exports = router;
